6a51308ee6f18858062883fa74d25b3ec124d870
trillobite <jparnell0@gmail.com>
2015-01-29 20:28:57 -0800

    working on the navigation buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,59 @@
 
 var project = {
 	body: function() {
+		var menuBoxCSS = {
+			'width': '100%',
+			'height': '40px',
+			'text-align': 'center',
+		};
+		var menuButtonHover = {
+			on: {
+				'background-color': '#ABABAB',
+			},
+			off: {
+				'background-color': 'white',
+			},
+		}
+		var menuButtonCSS = {
+			'font-family': 'arial',
+			'text-align': 'center',
+			'line-height': '40px',
+			'float': 'left',
+			'width': '130px',
+			'height': '40px',
+			'-moz-box-shadow': '0 4px 8px rgba(0,0,0,0.5)',
+			'-webkit-box-shadow': '0 4px 8px rgba(0,0,0,0.5)',
+			'box-shadow': '0 4px 8px rgba(0,0,0,0.5)',
+		};
+
+		var menu = $jConstruct('div').css(menuBoxCSS);
+		var menuItem1 = $jConstruct('div', {
+			text: 'Shop',
+			id: 'menuItem1',
+		}).css(menuButtonCSS).event('mouseover', function() {
+			$('#menuItem1').css(menuButtonHover.on);
+ 		}).event('mouseout', function() {
+			$('#menuItem1').css(menuButtonHover.off);
+		});
+		var menuItem2 = $jConstruct('div', {
+			text: 'About Us',
+			id: 'menuItem2',
+		}).css(menuButtonCSS).event('mouseover', function() {
+			$('#menuItem2').css(menuButtonHover.on);
+		}).event('mouseout', function() {
+			$('#menuItem2').css(menuButtonHover.off);
+		});
+		var menuItem3 = $jConstruct('div', {
+			text: 'Cart',
+			id: 'menuItem3',
+		}).css(menuButtonCSS).event('mouseover', function() {
+			$('#menuItem3').css(menuButtonHover.on);
+		}).event('mouseout', function() {
+			$('#menuItem3').css(menuButtonHover.off);
+		});
+
+		menu.addChild(menuItem1).addChild(menuItem2).addChild(menuItem3);
+
 		var image = $jConstruct('image', {
 			src: './css/images/hipsterlogogenerator_1422579646895.png',
 		}).css({
@@ -25,7 +78,7 @@ var project = {
 			'-moz-box-shadow': '0 4px 8px rgba(0,0,0,0.5)',
 			'-webkit-box-shadow': '0 4px 8px rgba(0,0,0,0.5)',
 			'box-shadow': '0 4px 8px rgba(0,0,0,0.5)',
-		}).addChild(header);
+		}).addChild(menu).addChild(header);
 	}
 }
 
